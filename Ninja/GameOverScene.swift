b4b0b1e7c20a9ce8f832deb7a1ac6939fb417e0c
David Pai <dbai2.0@gmail.com>
2017-06-05 11:50:42 +0800

    c1 from office

---
+++ Ninja/GameOverScene.swift
@@ -0,0 +1,49 @@
+//
+//  GameOverScene.swift
+//  NinjaGame
+//
+//  Created by GrandFunk on 2017/4/28.
+//  Copyright © 2017年 GrandFunk. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class GameOverScene: SKScene {
+    
+    init(size: CGSize, won:Bool) {
+        
+        super.init(size: size)
+        
+        // 1
+        backgroundColor = SKColor.white
+        
+        // 2
+        let message = won ? "你贏了！" : "你輸了！"
+        
+        // 3
+        let label = SKLabelNode(fontNamed: "Chalkduster")
+        label.text = message
+        label.fontSize = 40
+        label.fontColor = SKColor.black
+        label.position = CGPoint(x: size.width/2, y: size.height/2)
+        addChild(label)
+        
+        // 4
+        run(SKAction.sequence([
+            SKAction.wait(forDuration: 3.0),
+            SKAction.run() {
+                // 5
+                let reveal = SKTransition.flipHorizontal(withDuration: 0.5)
+                let scene = GameScene(size: size)
+                self.view?.presentScene(scene, transition:reveal)
+            }
+            ]))
+        
+    }
+    
+    // 6
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
